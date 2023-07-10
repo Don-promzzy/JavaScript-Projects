3caebe074a0597f835153485f971ca91bf64a85e
Promise Imafidon <promiseimafidon@programmer.net>
2023-07-10 22:34:17 +0400

    Data Structure Objects

---
+++ 5. Data-structures-objects/script.js
@@ -0,0 +1,19 @@
+// Objects
+var user = {
+  name: "John",
+  age: 34,
+  hobby: "Soccer",
+  isMarried: false,
+  spells: ["abrakadra", "shazam", "boo"],
+};
+
+var list = [
+  {
+    username: "andy",
+    password: "secre",
+  },
+  {
+    username: "jess",
+    password: "123",
+  },
+];
