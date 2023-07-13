43e29d9d0e30a0af982642f43fc3d5bc01ecfcb0
Promise Imafidon <promiseimafidon@programmer.net>
2023-07-13 22:17:48 +0400

    Dom selectors

---
+++ 8. Dom-selectors/script.js
@@ -0,0 +1,7 @@
+var hiText = document.querySelector("h1");
+var noExcuses = document.querySelector(".second");
+var li = document.querySelectorAll("li")[2];
+
+hiText.style.background = "red";
+noExcuses.innerHTML = "<h2>Time wait for no man!!!</h2>";
+li.className = "app";
